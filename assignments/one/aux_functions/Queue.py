480ec61996f9038c3caa1ac6b62bfcb06608220d
ovindar <m.l.h.litsios@student.tue.nl>
2024-02-07 10:36:39 +0100

    queue

--- assignments/one/aux_functions/Queue.py
+++ assignments/one/aux_functions/Queue.py
@@ -3,16 +3,13 @@ import random
 
 
 class Queue:
-    def __init__(self, length, width):
-        self.q = np.array((length, width))
+    def __init__(self, length, high, low):
+        self.q = np.random.randint(high=high, low=low, size=length)
 
     def __len__(self):
-        return self.q.shape[0]
-
-    def width(self):
-        return self.q.shape[1]
+        return self.q.shape
 
     def head(self):
-        h = self.q[1, :]
-        self.q = self.q[1:, :]
+        h = self.q[1]
+        self.q = self.q[1:]
         return h
