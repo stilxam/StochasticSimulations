1643511d8ee0e11b24344b5b40750f14248da9e5
ovindar <m.l.h.litsios@student.tue.nl>
2024-02-07 14:19:39 +0100

    did q2

--- assignments/one/aux_functions/Queue.py
+++ assignments/one/aux_functions/Queue.py
@@ -7,7 +7,7 @@ class Queue:
         if high == low:
             self.q = high * np.ones(length)
         else:
-            self.q = np.random.randint(high=high, low=low, size=length)
+            self.q = np.random.randint(high=high+1, low=low, size=length)
 
     def __len__(self):
         return self.q.shape[0]
@@ -17,18 +17,35 @@ class Queue:
         self.q = self.q[1:]
         return h
 
+    def is_singles(self):
+        indexes = np.where(self.q == 1)[0]
+        if len(indexes) > 0:
+            return True
+        else:
+            return False
+
+    def pop_singles(self):
+        indexes = np.where(self.q == 1)[0]
+        first_one = indexes[0]
+        self.q = np.concatenate([self.q[:first_one], self.q[first_one + 1:]])
+        return 1
+
+    def stack(self, head):
+        self.q = np.concatenate([np.array([head]), self.q])
+
     def __str__(self):
         return f"{self.q}"
 
 
 def main():
-    q = Queue(2, 5, 1)
+    q = Queue(20, 3, 1)
     print(q)
-    print(q.head())
+    print(q.singles())
     print(q)
-    print(q.head())
-    print(q)
+    # print(q)
+    # print(q.head())
+    # print(q)
 
 
-if __name__ =="__main__":
+if __name__ == "__main__":
     main()
--- assignments/one/one_q.py
+++ assignments/one/one_q.py
@@ -5,7 +5,6 @@ from joblib import Parallel, delayed
 import multiprocessing as mp
 import random
 import time
-from functools import partial
 from tqdm.auto import tqdm
 
 def simulate_filling_a_boat(queue: Queue, passengers):
@@ -19,8 +18,8 @@ def simulate_filling_a_boat(queue: Queue, passengers):
             boat.fill_boat(group)
             n_its += 1
         elif not boat.is_filling_possible(group):
+            queue.stack(group)
             break
-
     return queue
 
 
@@ -31,30 +30,23 @@ def sim_queue(len_q, max_group_s, min_group_s, passengers):
     while len(single_queue) > 0:
         single_queue = simulate_filling_a_boat(single_queue, passengers)
         total_its += 1
-
     return total_its/len_q
 
 
-def parallelized_q(n_runs=100000, len_q=100, max_group_s=8, min_group_s=8, passengers=16, n_jobs=mp.cpu_count()-1):
+def parallelized_base_q(n_runs=100000, len_q=100, max_group_s=8, min_group_s=1, passengers=8, n_jobs=mp.cpu_count()-1):
 
     t_init = time.time()
-    # pre_filled = partial(sim_queue, len_q, max_group_s, min_group_s, passengers)
-
-    # results = np.zeros(n_runs)
-    # for i in tqdm(range(n_runs)):
-    #     results[i] = sim_queue(len_q=len_q,max_group_s=max_group_s, min_group_s=min_group_s, passengers=passengers)
 
     results = Parallel(n_jobs=n_jobs)(delayed(sim_queue)(len_q, max_group_s, min_group_s, passengers) for _ in range(n_runs))
-    # results = Parallel(n_jobs=nr_cores_to_use)(delayed(simulate_game)(game_sim) for _ in range(nruns))
 
     t_term = time.time()
     results = np.array(results)
 
     print(f"Average Iterations for Q of len {len_q} is {results.mean()}, with VAR {results.var()}")
     print(f"Process took {t_term-t_init}")
+    return results
 
 
 
 if __name__ == "__main__":
-    # print(simulate_filling_a_boat(Queue(10, 8, 1), 8))
-    parallelized_q()
+    parallelized_base_q()
