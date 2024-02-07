50572a42514efd8532d74378fe84a034fe86ce23
ovindar <m.l.h.litsios@student.tue.nl>
2024-02-07 10:13:57 +0100

    initialized assignment one

---
+++ assignments/one/aux_functions/boat.py
@@ -0,0 +1,22 @@
+import numpy
+
+
+class Boat:
+    def __init__(self, n: int = 8):
+        self.n_seats: int = n
+        self.filled_seats: int = 0
+
+    def fill_boat(self, group:int):
+        self.filled_seats += group
+
+    def is_filling_possible(self, group:int):
+        if self.n_seats < self.filled_seats+group:
+            return False
+        elif self.n_seats >= self.filled_seats+group:
+            return True
+
+    def is_boat_full(self):
+        if self.n_seats < 8:
+            return False
+        elif self.n_seats == 8:
+            return True
