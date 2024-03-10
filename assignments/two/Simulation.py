e71e88a05ae86e312b9af869a19333cae5982be6
ovindar <m.l.h.litsios@student.tue.nl>
2024-03-10 13:51:14 +0100

    ????? maybe something

--- assignments/two/Simulation.py
+++ assignments/two/Simulation.py
@@ -15,13 +15,13 @@ class Simulation:
         self.lam = arrival_rate
         self.mu = departure_rate
         self.num_servers = num_servers
-        self.arrDist = stats.expon(scale = 1 / self.lam)
+        self.arrDist = stats.expon(scale=1 / self.lam)
         self.theta = theta
         self.T = max_time
         self.fes = FES()
-    
+
     def simulate(self):
-        t = 0   # current time
+        t = 0  # current time
 
         # create queues
         queues = []
@@ -31,7 +31,6 @@ class Simulation:
         # initialize the dispatcher
         dispatcher = Dispatcher(self.theta, self.num_servers)
 
-        
         # generate the first arrival
         a = self.arrDist.rvs()
         # we set the server_id to -1 to indicate that the event is not assigned to any server
@@ -46,8 +45,8 @@ class Simulation:
             t = event.time
 
             # update the surface below the queue length graph for all queues
-            for i in range (self.num_servers):
-                if (queues[i].num_customers == 0 ):
+            for i in range(self.num_servers):
+                if (queues[i].num_customers == 0):
                     queues[i].S += (t - tOld) * queues[i].num_customers
                     queues[i].add_area_to_history()
 
@@ -55,7 +54,6 @@ class Simulation:
                     queues[i].S += (t - tOld) * (queues[i].num_customers - 1)
                     queues[i].add_area_to_history()
 
-            
             if event.type == Event.ARRIVAL:
 
                 # accept or reject the arrival
@@ -68,21 +66,21 @@ class Simulation:
                     if queues[server_id].num_customers == 1:
                         dep = Event(Event.DEPARTURE, t + queues[server_id].servDist.rvs(), server_id)
                         self.fes.add(dep)
-                
+
                 a = self.arrDist.rvs()
                 arr = Event(Event.ARRIVAL, t + a, -1)
                 self.fes.add(arr)
-            
+
             elif event.type == Event.DEPARTURE:
                 # retrieve the server_id from the event
                 server_id = event.server_id
                 queues[server_id].num_customers -= 1
-                
+
                 # check if there are customes waiting for the server
                 if queues[server_id].num_customers > 0:
                     dep = Event(Event.DEPARTURE, t + queues[server_id].servDist.rvs(), server_id)
                     self.fes.add(dep)
-            
+
         results = []
         area_histories = []
         # print the surface below the queue length graph for all queues
@@ -90,23 +88,25 @@ class Simulation:
             results.append(queues[i].S / t)
             area_histories.append(queues[i].area_history)
 
-        
-        return results # area_histories
+        return results  # area_histories
 
     def get_state(self, queues):
         return np.array([cust.num_customers for cust in queues])
 
-    def apply_action_and_get_reward(self, gamma, action, queues):
+    def apply_action_and_get_reward(self, xi, action, queue):
         arrival_time = self.arrDist.rvs()
         arrival_event = Event(Event.ARRIVAL, self.T + arrival_time, -1)
         self.fes.add(arrival_event)
 
-        queues[action].num_customers += 1
+        if action == 1:
+            queue.num_customers += 1
+        else:
+            pass
 
         # Calculate the reward based on the outcome of the action
         # im not sure about this reward function
-        reward = gamma - sum(self.get_state(queues))
-        return queues, reward
+        reward = abs(xi - queue.num_customers)
+        return queue, reward
 
     def sarsa_loop(self, alpha, epsilon, gamma, lr, num_states, num_actions):
         t = 0
@@ -123,14 +123,14 @@ class Simulation:
 
         # add the first event to the FES
         self.fes.add(firstEvent)
-        
+
         # Pass the current state to the dispatcher to choose an action
         state = self.get_state(queues)
-        action = np.random.choice(num_actions+1)
+        action = np.random.choice(num_actions + 1)
 
         # Set the first state-action pair to 0
         dispatcher.Q[state, action] = 0
-        
+
         while t < self.T:
             tOld = t
             event = self.fes.next()
@@ -148,11 +148,11 @@ class Simulation:
 
             # Apply the action and observe the next state and reward
             queues, reward = self.apply_action_and_get_reward(gamma, action, queues)
-            
+
             # Pass this new state to the dispatcher to choose an action
             next_state = self.get_state(queues)
             next_action = dispatcher.choose_action(next_state)
-            
+
             if dispatcher.Q[next_state, next_action] == -np.infty:
                 dispatcher.Q[next_state, next_action] = 0
 
@@ -164,8 +164,74 @@ class Simulation:
             arr = Event(Event.ARRIVAL, t + a, -1)
             self.fes.add(arr)
 
+    def single_sarsa(self, alpha, epsilon, xi, lr, num_states, num_actions):
+        """
+        :param alpha: discount factor
+        :param epsilon: exploration probability
+        :param xi: goal
+        :param lr: learning rate
+        :param num_states: number of states
+        :param num_actions: number of actions
+        :return:
+        """
+
+        t = 0
+        queue = Queue(self.mu[0])
+
+        salsa_dancer = SARSA(
+            alpha=alpha,
+            epsilon=epsilon,
+            lr=lr,
+            num_states=num_states,
+            num_actions=num_actions
+        )
+
+        a = self.arrDist.rvs()
+        # we set the server_id to -1 to indicate that the event is not assigned to any server
+        firstEvent = Event(Event.ARRIVAL, a, -1)
+
+        # add the first event to the FES
+        self.fes.add(firstEvent)
+
+        # Pass the current state to the dispatcher to choose an action
+        state = queue.num_customers
+        action = np.random.choice(num_actions + 1)
+
+        # Set the first state-action pair to 0
+        salsa_dancer.Q[state, action] = 0
+
+        while t < self.T:
+            start_t = t
+            previous_state = state
+            previous_action = action
+
+            event = self.fes.next()
+            t = event.time()
+
+            if queue.num_customers == 0:
+                queue.S += (t - start_t) * queue.num_customers
+                queue.add_area_to_history()
+
+            elif queue.num_customers > 0:
+                queue.S += (t - start_t) * (queue.num_customers - 1)
+                queue.add_area_to_history()
+
+            action = salsa_dancer.choose_action(queue.num_customers)
+
+            queue, reward = self.apply_action_and_get_reward(xi=xi, action=action, queue=queue)
+
+            state = queue.num_customers
+
+            salsa_dancer.update(
+                previous_state,
+                previous_action,
+                reward,
+                state,
+                action
+            )
+
 
 if __name__ == "__main__":
     np.random.seed(None)
-    sim = Simulation (2, [3,4], 2, 0.4, 10000)
+    sim = Simulation(2, [3, 4], 2, 0.4, 10000)
     sim.simulate()
--- assignments/two/rl.py
+++ assignments/two/rl.py
@@ -8,11 +8,10 @@ class SARSA():
         self.alpha: float = alpha  # discount factor
         self.epsilon: float = epsilon  # exploration probabilities
         self.lr: float = lr  # learning rate
-
         self.num_states: int = num_states
         self.num_actions: int = num_actions
 
-        self.Q = np.full((num_states, num_actions+1), -np.infty)  #TODO: I DONT KNOW WHAT STRUCTURE TO GIVE THIS
+        self.Q = np.full((num_states, num_actions), -np.infty)  #TODO: I DONT KNOW WHAT STRUCTURE TO GIVE THIS
 
     def choose_action(self, state):
         if np.random.uniform(0, 1) < self.epsilon:
@@ -24,11 +23,24 @@ class SARSA():
         """Update the Q-value for the given state-action pair."""
         update_value = self.Q[state, action]
         new_value = self.Q[next_state, next_action]
-        # next_max = np.max(self.Q[next_state]) # STRUCTURE WILL HAVE IMPACT HERE
 
         # SARSA update rule
         update_value = (1 - self.lr) * (update_value) + self.lr * (reward + self.alpha * new_value)
         # new_value = (1 - self.lr) * (old_value) + self.lr *(reward + self.alpha * next_max)
         self.Q[state, action] = update_value
+
+
+
+
+
+def main():
+    sarsa = SARSA(
+        alpha=0.5,
+        epsilon=0.1,
+        lr=0.01,
+        num_states=5,
+        num_actions=2
+    )
+
 
 
